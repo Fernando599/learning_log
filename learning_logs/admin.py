059755c2565db712ffc0d74c9c08099d75030ad4
Fernando599 <kevinmafra1@gmail.com>
2025-09-26 08:31:30 -0300

    adicionei o modelo Entry e fiz algumas configurações no django admin

--- learning_logs/admin.py
+++ learning_logs/admin.py
@@ -3,5 +3,13 @@ from learning_logs import models
 
 @admin.register(models.Topic)
 class TopicAdmin(admin.ModelAdmin):
-    list_display = ('id', 'text', 'created_date',)
-    search_fields = ('text',)
+    list_display = ('id', 'name', 'created_date')
+    search_fields = ('name',)
+
+@admin.register(models.Entry)
+class EntryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'topic', 'created_date', 'resumo_texto')
+
+    def resumo_texto(self, obj):
+        return obj.text[:50] + "..." if len(obj.text) > 50 else obj.text
+    resumo_texto.short_description = "Text"
--- learning_logs/models.py
+++ learning_logs/models.py
@@ -1,8 +1,20 @@
 from django.db import models
 
 class Topic(models.Model):
-    text = models.CharField(max_length=200)
+    name = models.CharField(max_length=200)
     created_date = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.name
+    
+class Entry(models.Model):
+    topic = models.ForeignKey(Topic, on_delete=models.PROTECT)
+    created_date = models.DateTimeField(auto_now_add=True)
+    text = models.TextField()
+
+    class Meta:
+        verbose_name = 'Entry'
+        verbose_name_plural = 'Entries'
 
     def __str__(self):
-        return self.text
+        return self.text[:50]+'...'
