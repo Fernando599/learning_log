92d23e26942e646e698456f01fe34b0b3f1f90f2
Fernando599 <kevinmafra1@gmail.com>
2025-09-27 10:28:54 -0300

    criei o formulário para criar novos tópicos.

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
-from .models import Topic
+from.models import Topic
+from.forms import TopicForm
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
 def index(request):
     return render(
@@ -24,4 +27,18 @@ def topic(request, topic_id):
         request,
         'learning_logs/topic.html',
         {'topic': topic, 'entries': entries,}
+    )
+
+def new_topic(request):
+    if request.method != 'POST':
+        form = TopicForm()
+    else:
+        form = TopicForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('topics'))
+    return render(
+        request,
+        'learning_logs/new_topic.html',
+        {'form': form}
     )
