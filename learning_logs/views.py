5c5cf493d29f4405369e41576fe73ed95a72ea34
Fernando599 <kevinmafra1@gmail.com>
2025-09-27 11:39:49 -0300

    criei o formuário para novas entradas.

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
-from.models import Topic
-from.forms import TopicForm
+from .models import Topic
+from .forms import TopicForm, EntryForm
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 
@@ -41,4 +41,24 @@ def new_topic(request):
         request,
         'learning_logs/new_topic.html',
         {'form': form}
+    )
+
+def new_entry(request, topic_id):
+    topic = Topic.objects.get(id=topic_id)
+
+    if request.method != 'POST':
+        form = EntryForm()
+    else:
+        form = EntryForm(data=request.POST)
+        if form.is_valid():
+            new_entry = form.save(commit=False)
+            new_entry.topic = topic
+            new_entry.save()
+            return HttpResponseRedirect(reverse('topic', args=[topic_id]))
+        
+
+    return render(
+        request,
+        'learning_logs/new_entry.html',
+        {'topic': topic, 'form': form}
     )
